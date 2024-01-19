0a80eaddc3980d0815c47313b9cb2095dc49c818
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-01-19 22:48:39 +0600

    final commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,5 +51,7 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
